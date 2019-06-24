cf5cefe601a8ac848f49e4fd0c9522bf08d5408d
alexandersperling <alexander@reinelt.de.com>
2019-06-24 20:46:30 +0200

    use real commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ DOMAIN=$1
 chmod 600 temp_id_rsa
 mkdir /root/.ssh
 ssh-keyscan sprlng.de > /root/.ssh/known_hosts
-echo ssh -i temp_id_rsa yolped@sprlng.de "rm -r /var/www/$DOMAIN/*" || true
+ssh -i temp_id_rsa yolped@sprlng.de "rm -r /var/www/$DOMAIN/*" || true
 echo "########## Deploying ${CI_COMMIT_TAG} to prod ##########"
-echo scp -r -i temp_id_rsa ./src "yolped@sprlng.de:/var/www/$DOMAIN"
+scp -r -i temp_id_rsa ./src "yolped@sprlng.de:/var/www/$DOMAIN"
 rm -rf temp_id_rsa
