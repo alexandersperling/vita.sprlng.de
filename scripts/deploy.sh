70f354c233169189a8d56c748a01e5ea98b8ee8b
alexandersperling <alexander@reinelt.de.com>
2019-12-30 17:07:19 +0100

    Adapt path to copy source code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ chmod 600 temp_id_rsa
 mkdir /root/.ssh
 ssh-keyscan vision.sprlng.de > /root/.ssh/known_hosts
 echo "########## Deploying ${CI_COMMIT_SHORT_SHA} to prod ##########"
-scp -r -i temp_id_rsa ./src "yolped@vision.sprlng.de:/var/www/$DOMAIN"
+scp -r -i temp_id_rsa ./src "yolped@vision.sprlng.de:/home/yolped/services/$DOMAIN"
 ssh -i temp_id_rsa yolped@vision.sprlng.de "cd ~/services/$DOMAIN && docker-compose down && docker-compose pull && docker-compose up -d"
 rm -rf temp_id_rsa
