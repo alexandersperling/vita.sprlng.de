614a126bc9a6872b4fd5b503ed878bc7c7c96250
alexandersperling <alexander@reinelt.de.com>
2019-12-30 16:37:13 +0100

    fix typo in hostname

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 DOMAIN=$1
 chmod 600 temp_id_rsa
 mkdir /root/.ssh
-ssh-keyscan visition.sprlng.de > /root/.ssh/known_hosts
+ssh-keyscan vision.sprlng.de > /root/.ssh/known_hosts
 echo "########## Deploying ${CI_COMMIT_SHORT_SHA} to prod ##########"
 scp -r -i temp_id_rsa ./src "yolped@vision.sprlng.de:/var/www/$DOMAIN"
 ssh -i temp_id_rsa yolped@vision.sprlng.de "cd ~/services/$DOMAIN && docker-compose down && docker-compose pull && docker-compose up -d"
