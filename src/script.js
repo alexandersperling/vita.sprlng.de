bce997f981dd07c6e09679b8fca71f09abaf66c4
Alexander Sperling <alexander@sprlng.de>
2021-04-03 09:50:39 +0000

    Jobupdate

--- src/script.js
+++ src/script.js
@@ -14,5 +14,7 @@ $(document).ready(function(){
     $("#fuentis").click(function(){
         $("#exp5").toggle();
     });
-
+    $("#db").click(function(){
+        $("#exp6").toggle();
+    });
 });
